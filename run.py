a1ba357bbf7874ba5e6545edf18a0af6d2370372
Nanohana <56324869+GabrielDornelles@users.noreply.github.com>
2020-11-04 23:54:41 -0300

    add queue and basic features

--- run.py
+++ run.py
@@ -12,19 +12,19 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
+    'restrictfilenames': False,
+    'noplaylist': False,
     'nocheckcertificate': True,
-    'ignoreerrors': False,
+    'ignoreerrors': True,
     'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0' # bind to ipv4
 }
 
 ffmpeg_options = {
-    'options': '-vn'
+    'options': ''
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
@@ -50,49 +50,32 @@ class YTDLSource(discord.PCMVolumeTransformer):
         filename = data['url'] if stream else ytdl.prepare_filename(data)
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
-
 client = commands.Bot(command_prefix='Seulgi ')
 
-status = ['🦋 𝕯𝖔𝖜𝖔𝖗𝖓𝖊𝖑𝖑𝖊𝖘 🦋 assistant', 'Um mosquitinho', 'Seulgi queue, Seulgi play']
+status = ['🦋 𝕯𝖔𝖜𝖔𝖗𝖓𝖊𝖑𝖑𝖊𝖘 🦋 assistant', 'Um mosquitinho', 'Seulgi play ~Song Name~']
 queue = []
 
 @client.event
 async def on_ready():
-    change_status.start()
     print('Bot is online!')
 
 @client.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name='general')
-    await channel.send(f'Welcome {member.mention}!  Ready to jam out? See `?help` command for details!')
+    await channel.send(f'Welcome {member.mention}!  Ready to jam out? See `Seulgi help` command for details!')
         
-@client.command(name='ping', help='This command returns the latency')
+@client.command(name='ping', help='Seulgi returns your latency')
 async def ping(ctx):
     await ctx.send(f'**Pong!** Latency: {round(client.latency * 1000)}ms')
 
-@client.command(name='hello', help='This command returns a random welcome message')
+@client.command(name='hello', help='Seulgi choose a random hello message for you')
 async def hello(ctx):
     responses = ['***grumble*** Why did you wake me up?', 'Top of the morning to you lad!', 'Hello, how are you?', 'Hi', '**Wasssuup!**']
     await ctx.send(choice(responses))
 
-@client.command(name='die', help='This command returns a random last words')
-async def die(ctx):
-    responses = ['why have you brought my short life to an end', 'i could have done so much more', 'i have a family, kill them instead']
-    await ctx.send(choice(responses))
-
-@client.command(name='credits', help='This command returns the credits')
-async def credits(ctx):
-    await ctx.send('Made by `RK Coding`')
-    await ctx.send('Thanks to `DiamondSlasher` for coming up with the idea')
-    await ctx.send('Thanks to `KingSticky` for helping with the `?die` and `?creditz` command')
-
 @client.command(name='teamo', help='This returns the true love')
 async def teamo(ctx):
     await ctx.send('tb te amo <3')
-
-@client.command(name='creditz', help='This command returns the TRUE credits')
-async def creditz(ctx):
-    await ctx.send('**No one but me, lozer!**')
 
 @client.command(name='join', help='This command makes the bot join the voice channel')
 async def join(ctx):
@@ -102,39 +85,53 @@ async def join(ctx):
     
     else:
         channel = ctx.message.author.voice.channel
-
     await channel.connect()
-
+    
 @client.command(name='queue', help='This command adds a song to the queue')
-async def queue_(ctx, url):
+async def queue_(ctx, url, *args):
     global queue
+    helptext = url
+    for word in args:
+        helptext+= word
 
-    queue.append(url)
-    await ctx.send(f'`{url}` added to queue!')
+    queue.append(helptext)
+    await ctx.send(f'`{queue[0]}` added to queue!')
 
 @client.command(name='remove', help='This command removes an item from the list')
 async def remove(ctx, number):
     global queue
-
     try:
         del(queue[int(number)])
+        
         await ctx.send(f'Your queue is now `{queue}!`')
-    
+
     except:
         await ctx.send('Your queue is either **empty** or the index is **out of range**')
-        
+
 @client.command(name='play', help='This command plays songs')
-async def play(ctx,url=None):
+async def play(ctx, url=None,*args):
     global queue
+    try:
+        await ctx.invoke(client.get_command('join'))
+    except:
+        pass #Seulgi is already at the channel
 
     server = ctx.message.guild
     voice_channel = server.voice_client
-    if url is not None:
-        queue.append(url)
+    
+    helptext = url
+    for word in args:
+        helptext+= ' '+word
+
+    queue.append(helptext)
+    if voice_channel.is_playing():
+        await ctx.send(f'`{queue[len(queue)-1]}` added to queue!')
+        return
+
     async with ctx.typing():
         player = await YTDLSource.from_url(queue[0], loop=client.loop)
         voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
+        playing.start(ctx)
     await ctx.send('**Now playing:** {}'.format(player.title))
     del(queue[0])
 
@@ -142,14 +139,12 @@ async def play(ctx,url=None):
 async def pause(ctx):
     server = ctx.message.guild
     voice_channel = server.voice_client
-
     voice_channel.pause()
 
 @client.command(name='resume', help='This command resumes the song!')
 async def resume(ctx):
     server = ctx.message.guild
     voice_channel = server.voice_client
-
     voice_channel.resume()
 
 @client.command(name='view', help='This command shows the queue')
@@ -167,9 +162,18 @@ async def stop(ctx):
     voice_channel = server.voice_client
     voice_channel.stop()
 
-@tasks.loop(seconds=20)
-async def change_status():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=choice(status)))
-   #await client.change_presence(activity=discord.game(choice(status)))
-
-client.run('token')
+@tasks.loop(seconds=1)
+async def playing(ctx):
+    global queue
+    voice_channel = ctx.message.guild.voice_client
+    if(voice_channel.is_playing() is False):
+        async with ctx.typing():
+            player = await YTDLSource.from_url(queue[0], loop=client.loop)
+            voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        await ctx.send('**Now playing:** {}'.format(player.title))
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=('{}'.format(player.title))))
+        del (queue[0])
+    else:
+        pass
+      
+client.run('')
